80a33f519f699f11eba5396ac8c4d4322be08ddb
Wojtek <wo.przybyo@gmail.com>
2021-12-30 21:25:33 +0100

    TCPMES-10 Add first working version of TCP Messenger

--- Projects/STM32F769I-Discovery/Applications/LwIP/LwIP_HTTP_Server_Socket_RTOS/Src/httpserver-socket.c
+++ Projects/STM32F769I-Discovery/Applications/LwIP/LwIP_HTTP_Server_Socket_RTOS/Src/httpserver-socket.c
@@ -49,179 +49,110 @@
 #include "server-socket.h"
 #include "cmsis_os.h"
 #include "lcd_log.h"
-//#include "uart_api.h"
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
 
-/* Private typedef -----------------------------------------------------------*/
-/* Private define ------------------------------------------------------------*/
-#define SERVER_SOCKET_THREAD_PRIO (osPriorityHigh)
-#define CLIENT_SOCKET_THREAD_PRIO (osPriorityHigh)
+#define FIRST_CLIENT_SOCKET_THREAD_PRIO (osPriorityHigh)
+#define SECOND_CLIENT_SOCKET_THREAD_PRIO (osPriorityHigh)
 
-#define RX_BUFFER_SIZE 128
 #define MSG_BUFFER_SIZE 128
 
-static uint8_t msg_buf[MSG_BUFFER_SIZE + 2];
-static int32_t msg_buf_cnt;
+static uint8_t msg_buf[MSG_BUFFER_SIZE + 2], client_recv_buf[MSG_BUFFER_SIZE + 2];
+static int32_t msg_buf_cnt, client_recv_buf_cnt;
+static volatile bool client_running, msg_received;
+int Client1, Client2, ConnFirstSocket, ConnSecondSocket, Recived, size;
 
-static int8_t client_recv_buf[MSG_BUFFER_SIZE + 2];
-static int32_t client_recv_buf_cnt;
-static volatile bool client_running;
-static volatile bool msg_received;
-/* Private function prototypes -----------------------------------------------*/
-/* Private functions ---------------------------------------------------------*/
-static void client_socket_write_thread(void *arg);
-static void client_socket_read_thread(void *arg);
-
-void socket_serve(int conn)
+// FIRST SOCKET
+// ============
+void FirstSocket(int conn)
 {
   while (1)
   {
-    int n_rec;
-    while (msg_received == true)
-    {
-    }
-
-    /* Read in the request */
-    n_rec = read(conn, msg_buf, MSG_BUFFER_SIZE);
-    if (n_rec < 0)
+    Recived = read(conn, msg_buf, MSG_BUFFER_SIZE);
+    if (Recived < 0)
       return;
 
-    msg_buf[n_rec] = '\0';
-    LCD_UsrLog("Otrzymano wiadomosc: %s\n", &msg_buf[0]);
-    msg_buf[n_rec] = '\r';
-    msg_buf[n_rec + 1] = '\n';
-    msg_buf_cnt = n_rec + 2;
+    msg_buf[Recived] = '\0';
+    LCD_UsrLog("Otrzymano wiadomosc: %s\n", &msg_buf[0]); // TODO ... wiadomosc od [UZUPELNIC]
+    msg_buf[Recived] = '\n';
+    msg_buf_cnt = Recived + 2;
 
-    // msg_received = true;
-    write(conn, &msg_buf[0], n_rec + 2);
-
-    /* Close connection socket */
-    // close(conn);
+    write(ConnSecondSocket, &msg_buf[0], Recived + 2);
   }
 }
 
-static void server_socket_thread(void *arg)
+// SECOND SOCKET
+// =============
+void SecondSocket(int conn)
 {
-  int sock, newconn, size;
+  while (1)
+  {
+    Recived = read(conn, msg_buf, MSG_BUFFER_SIZE);
+    if (Recived < 0)
+      return;
+
+    msg_buf[Recived] = '\0';
+    LCD_UsrLog("Otrzymano wiadomosc: %s\n", &msg_buf[0]); // TODO ... wiadomosc od [UZUPELNIC]
+    msg_buf[Recived] = '\r';
+    msg_buf_cnt = Recived + 2;
+
+    write(ConnFirstSocket, &msg_buf[0], Recived + 2);
+  }
+}
+
+// First Socket Thread --> Port 80
+// ===============================
+static void FirstClientThread(void *arg)
+{
   struct sockaddr_in address, remotehost;
 
-  msg_received = false;
-  /* create a TCP socket */
-  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-  {
+  if ((Client1 = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     return;
-  }
 
-  /* bind to port 80 at any interface */
   address.sin_family = AF_INET;
   address.sin_port = htons(80);
   address.sin_addr.s_addr = INADDR_ANY;
 
-  if (bind(sock, (struct sockaddr *)&address, sizeof(address)) < 0)
-  {
+  if (bind(Client1, (struct sockaddr *)&address, sizeof(address)) < 0)
     return;
-  }
 
-  /* listen for incoming connections (TCP listen backlog = 5) */
-  listen(sock, 5);
-
+  listen(Client1, 1);
   size = sizeof(remotehost);
 
-  while (1)
-  {
-    newconn = accept(sock, (struct sockaddr *)&remotehost, (socklen_t *)&size);
-    socket_serve(newconn);
-  }
+  ConnFirstSocket = accept(Client1, (struct sockaddr *)&remotehost, (socklen_t *)&size);
+  FirstSocket(ConnFirstSocket);
 }
-static void client_socket_read_thread(void *arg)
+
+// Second Socket Thread --> Port 90
+// ================================
+static void SecondClientThread(void *arg)
 {
-  int sock = *((int *)arg);
-  client_recv_buf_cnt = 0;
-  while (client_running)
-  {
-    while (client_recv_buf_cnt > 0)
-    {
-    }
-    client_recv_buf_cnt = read(sock, &client_recv_buf[0], MSG_BUFFER_SIZE);
-  }
+  struct sockaddr_in address, remotehost;
 
-  osThreadTerminate(NULL);
+  if ((Client2 = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    return;
+
+  address.sin_family = AF_INET;
+  address.sin_port = htons(90);
+  address.sin_addr.s_addr = INADDR_ANY;
+
+  if (bind(Client2, (struct sockaddr *)&address, sizeof(address)) < 0)
+    return;
+
+  listen(Client2, 1);
+  size = sizeof(remotehost);
+
+  ConnSecondSocket = accept(Client2, (struct sockaddr *)&remotehost, (socklen_t *)&size);
+  SecondSocket(ConnSecondSocket);
 }
 
-static void client_socket_write_thread(void *arg)
-{
-  static int8_t uart_buf[RX_BUFFER_SIZE + 1];
-  int32_t recv_cnt = 0;
-  const int port = 1007;
-  int sock;
-  struct sockaddr_in chat_server_addr;
-  client_running = true;
-
-  /* create a TCP socket */
-  if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
-  {
-    LCD_UsrLog("client: Error creating socket\n");
-    client_running = false;
-    osThreadTerminate(NULL);
-  }
-
-  // bzero(&chat_server_addr, sizeof(chat_server_addr));
-  memset(&chat_server_addr, 0, sizeof(chat_server_addr));
-
-  chat_server_addr.sin_family = AF_INET;
-  inet_pton(AF_INET, "192.168.10.5", &chat_server_addr.sin_addr);
-  chat_server_addr.sin_port = htons(port);
-  while (connect(sock, (struct sockaddr *)&chat_server_addr, sizeof(chat_server_addr)) < 0)
-  {
-    LCD_UsrLog("client: Error connecting\n");
-    client_running = false;
-    osThreadTerminate(NULL);
-  }
-  sys_thread_new("client_read", client_socket_read_thread, &sock, DEFAULT_THREAD_STACKSIZE * 2, CLIENT_SOCKET_THREAD_PRIO);
-  while (1)
-  {
-    if (msg_received)
-    {
-      if (0 == strncmp(&msg_buf[0], "Close client", 12))
-      {
-        LCD_UsrLog("Closing client\n");
-        close(sock);
-        break;
-      }
-      LCD_UsrLog("Sending %ld chars as gateway\n", msg_buf_cnt);
-      write(sock, &msg_buf[0], msg_buf_cnt);
-      msg_received = false;
-    }
-    /*
-    if ((recv_cnt = UartReadCommand(&uart_buf[0], RX_BUFFER_SIZE)) > 0)
-    {
-      uart_buf[recv_cnt] = '\0';
-      LCD_UsrLog("UART: %s\r\n", uart_buf);
-      uart_buf[recv_cnt] = '\r';
-      uart_buf[recv_cnt + 1] = '\n';
-      write(sock, &uart_buf[0], recv_cnt + 2);
-    }
-    */
-    if (client_recv_buf_cnt > 0)
-    {
-      client_recv_buf[client_recv_buf_cnt] = '\0';
-      LCD_UsrLog("Client read: %s\r\n", client_recv_buf);
-      client_recv_buf[client_recv_buf_cnt] = '\r';
-      client_recv_buf[client_recv_buf_cnt] = '\n';
-      //  UartWriteCommand(&client_recv_buf[0], client_recv_buf_cnt + 2);
-      client_recv_buf_cnt = 0;
-    }
-  }
-  client_running = false;
-  osThreadTerminate(NULL);
-}
-
+// Threads initialization
+// ======================
 void server_socket_init()
 {
-  sys_thread_new("server", server_socket_thread, NULL, DEFAULT_THREAD_STACKSIZE * 2, SERVER_SOCKET_THREAD_PRIO);
-  sys_thread_new("client_write", client_socket_write_thread, NULL, DEFAULT_THREAD_STACKSIZE * 2, CLIENT_SOCKET_THREAD_PRIO);
+  sys_thread_new("FirstClient", FirstClientThread, NULL, DEFAULT_THREAD_STACKSIZE * 2, FIRST_CLIENT_SOCKET_THREAD_PRIO);
+  sys_thread_new("SecondClient", SecondClientThread, NULL, DEFAULT_THREAD_STACKSIZE * 2, SECOND_CLIENT_SOCKET_THREAD_PRIO);
 }
 
 /************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
