613a425f8b405e03f4fed84cda67e6fd1b7aeb85
Artur Nowak <arturmareknowal@gmail.com>
2022-01-20 23:13:24 +0100

    TCPMES-13 fixed the garbage in the buffer

--- CLIENT/client.cpp
+++ CLIENT/client.cpp
@@ -28,7 +28,11 @@ DWORD WINAPI ReadThread(void* args)
 	    n = read(sockfd, buffer, 255);
         if (n < 0) 
             error("ERROR reading from socket");
-        printf("%s\n", buffer);
+
+	//for(int i = 0; i < 2; i++)
+	//	printf("%c", buffer[i]);
+	printf("%.*s", n, buffer);
+
 	}
 	return 5;
 }
