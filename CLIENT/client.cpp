2c16b34056e81302db6c358b7fc6aa86e3a364c5
Wojtek <wo.przybyo@gmail.com>
2022-01-22 00:39:49 +0100

    TCPMES-18 Small client code refactoring

--- CLIENT/client.cpp
+++ CLIENT/client.cpp
@@ -16,77 +16,81 @@ void error(const char *msg)
     exit(0);
 }
 
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
-	    char buffer[256];
+int sockfd, portno, n;
+struct sockaddr_in serv_addr struct hostent *server;
+char buffer[256];
 
-DWORD WINAPI ReadThread(void* args)
+DWORD WINAPI ReadThread(void *args)
 {
-	while(1)
-	{
-	    n = read(sockfd, buffer, 255);
+    while (1)
+    {
+        n = read(sockfd, buffer, 255);
 
-        if (n < 0) 
+        if (n < 0)
             error("ERROR reading from socket");
 
-	char *ctime_no_newline;
-    	time_t tm = time(NULL);
+        char *ctime_no_newline;
+        time_t tm = time(NULL);
 
-    	ctime_no_newline = strtok(ctime(&tm), "\n");
-    	printf("Otrzymano wiadomość %s", ctime_no_newline);
-	printf(" - %.*s", n - 1, buffer);
-
-	}
-	return 5;
+        ctime_no_newline = strtok(ctime(&tm), "\n");
+        printf("Message recived %s", ctime_no_newline);
+        printf(" - %.*s", n - 1, buffer);
+    }
+    return 1;
 }
 
-DWORD WINAPI WriteThread(void* args)
+DWORD WINAPI WriteThread(void *args)
 {
-	while(1)
-	{
-        bzero(buffer,256);
-        fgets(buffer,255,stdin);
+    while (1)
+    {
+        bzero(buffer, 256);
+        fgets(buffer, 255, stdin);
         n = write(sockfd, buffer, strlen(buffer));
 
-        if (n < 0) 
+        if (n < 0)
             error("ERROR writing to socket");
 
-        bzero(buffer,256);
-	}
-	return 5;
+        bzero(buffer, 256);
+    }
+    return 1;
 }
 
 int main(int argc, char *argv[])
 {
-HANDLE thread1, thread2;
+    HANDLE thread1, thread2;
 
-    if (argc < 3) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
+    if (argc < 3)
+    {
+        fprintf(stderr, "usage %s hostname port\n", argv[0]);
+        exit(0);
     }
     portno = atoi(argv[2]);
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
+    if (sockfd < 0)
         error("ERROR opening socket");
+
     server = gethostbyname(argv[1]);
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
+    if (server == NULL)
+    {
+        fprintf(stderr, "ERROR, no such host\n");
         exit(0);
     }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-	
+
+    bzero((char *)&serv_addr, sizeof(serv_addr));
+
     serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-		 
+    bcopy((char *)server->h_addr,
+          (char *)&serv_addr.sin_addr.s_addr,
+          server->h_length);
+
     serv_addr.sin_port = htons(portno);
-    if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) 
+    if (connect(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)
         error("ERROR connecting");
-	
-	thread1 = CreateThread(nullptr, 0, ReadThread, nullptr, 0 , nullptr);
-	thread2 = CreateThread(nullptr, 0, WriteThread, nullptr, 0 , nullptr);
-	
-	while(1){}
+
+    thread1 = CreateThread(nullptr, 0, ReadThread, nullptr, 0, nullptr);
+    thread2 = CreateThread(nullptr, 0, WriteThread, nullptr, 0, nullptr);
+
+    while (1)
+    {
+    }
 }
